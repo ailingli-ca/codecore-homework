ba746a8a57169f504057bdbb4d86237596c3f98a
Ailing Li <alofe9859@gmail.com>
2022-06-08 14:03:00 +0000

    write 3 functions: drawTopBorder, drawMiddleBorder and drawBottomBorder

--- box_it_script/solution.js
+++ box_it_script/solution.js
@@ -1,8 +1,37 @@
 function drawLine (num) {  
     let line = []
+    const base = "\u2501"
     for (let i = 0; i < num; i++){
         line += "─"
     }
     return line;
 }
-console.log(drawLine(4));
+//console.log(drawLine(4)); //for check
+
+function drawTopBorder (num) {    
+    const left = "\u250F";
+    let line = drawLine(num);
+    const right = "\u2513";
+    return left + line + right;
+}
+
+function drawMiddleBorder (num) {
+    const left = "\u2523";
+    let line = drawLine(num);
+    const right = "\u252B";
+    return left + line + right;
+}
+
+function drawBottomBorder (num) {
+    const left = "\u2517";
+    let line = drawLine(num);
+    const right = "\u251B";
+    return left + line + right;
+}
+
+
+
+
+
+
+
