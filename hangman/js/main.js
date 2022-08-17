d09e8241f227768aa7c4d99cb3c5f4c619a90ddb
Ailing Li <alofe9859@gmail.com>
2022-08-17 20:33:49 +0000

    Ending the Game & Stretch 1

--- hangman/js/main.js
+++ hangman/js/main.js
@@ -1,25 +1,44 @@
 $(document).ready(() => {
-  const mysteryWord = "STRANGER";
-
+  const words = ["ARCHERY", "BADMINTON", "BASKETBALL", "BASEBALL", "BOXING", "CLIMBING", "CURLING", "FENCING", "FOOTBALL", "GOLF", "HOCKEY", "SHOOTING", "SKATING", "SKATEBOARDING", "SKIING", "SURFING", "SWIMMING", "TENNIS", "VOLLEYBALL", "YOGA"];
+  const randomWord = words[Math.floor(Math.random() * words.length)];
+  
   let incorrects = 0;
   let corrects = 0;
+  const Total_Chances = 6;
 
-  for (let i = 0; i < mysteryWord.length; i++) {
-    $("table.mysteryWord tr").append($(`<td></td>`));
+  const victorySound = new Audio("sounds/victory.wav");
+  const failSound = new Audio("sounds/failure.wav");
+
+  for (let i = 0; i < randomWord.length; i++) {
+    $("table.randomWord tr").append($(`<td></td>`));
   }
 
   function checkGuessLetter(guess) {
-    if (mysteryWord.includes(guess)) {
-      let i = mysteryWord.indexOf(guess, 0);
-      while (i != -1) {
-        $(`table.mysteryWord td:nth-child(${i+1})`).text(guess);
-        i = mysteryWord.indexOf(guess, i + 1);
+    if (randomWord.includes(guess)) {
+      let i = randomWord.indexOf(guess, 0);
+      while (i != -1 && i < randomWord.length) {
+        corrects++;
+        $(`table.randomWord td:nth-child(${i + 1})`).text(guess);
+        i = randomWord.indexOf(guess, i + 1);
       }
-    }
-    else {
+      if (corrects === randomWord.length) {
+        endingGame(victorySound, "Congratulations! You win!");
+      }
+    } else {
       incorrects += 1;
       $("img.hangmanImage").attr('src', `images/${incorrects}.png`);
+      if (incorrects === Total_Chances) {
+        endingGame(failSound, "Better luck next time...");
+      }
     }
+  }
+
+  function endingGame(music, message){
+    music.play();
+    setTimeout(function(){
+      alert(message);
+      location.reload(); 
+   }, 1000);
   }
 
   $("table.letters td").on("click", event => {
