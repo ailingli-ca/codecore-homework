891e66621d64baeb4b5bc0fbbcfac6325557a3de
Ailing Li <alofe9859@gmail.com>
2022-08-17 05:26:37 +0000

    Correct & Incorrect Guesses

--- hangman/js/main.js
+++ hangman/js/main.js
@@ -1,5 +1,30 @@
 $(document).ready(() => {
-    $("table.letters td").on("click", event => {
-      $(event.currentTarget).addClass("selected");
-    });
+  const mysteryWord = "STRANGER";
+
+  let incorrects = 0;
+  let corrects = 0;
+
+  for (let i = 0; i < mysteryWord.length; i++) {
+    $("table.mysteryWord tr").append($(`<td></td>`));
+  }
+
+  function checkGuessLetter(guess) {
+    if (mysteryWord.includes(guess)) {
+      let i = mysteryWord.indexOf(guess, 0);
+      while (i != -1) {
+        $(`table.mysteryWord td:nth-child(${i+1})`).text(guess);
+        i = mysteryWord.indexOf(guess, i + 1);
+      }
+    }
+    else {
+      incorrects += 1;
+      $("img.hangmanImage").attr('src', `images/${incorrects}.png`);
+    }
+  }
+
+  $("table.letters td").on("click", event => {
+    $(event.currentTarget).addClass("selected");
+    checkGuessLetter($(event.currentTarget).text());
+  });
+
 });
